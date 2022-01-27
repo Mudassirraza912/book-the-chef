55b88bf3fcf6ae64a3b3c4ec6997a47f350cf124
Techsitrr <techstirr@Techsitrrs-iMac.local>
2022-01-27 13:59:59 +0500

    final

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ const app = express();
 const typeDefs = require('./Api/typeDefs');
 const resolvers = require('./Api/resolvers');
 
+app.get("/confirmation/:email/:token", async (req,res) => {
+    
+    Token.findOne({ token: req.params.token }, function (err, token) {
+        // token is not found into database i.e. token may have expired 
+        if (!token){
+            return res.status(400).send({msg:'Your verification link may have expired. Please click on resend for verify your Email.'});
+        }
+        // if token is found then check valid user 
+        else{
+            Resturant.findOne({ _id: token._userId, email: req.params.email }, function (err, user) {
+                // not valid user
+                if (!user){
+                    return res.status(401).send({msg:'We were unable to find a user for this verification. Please SignUp!'});
+                } 
+                // user is already verified
+                else if (user.verified){
+                    return res.status(200).send('User has been already verified. Please Login');
+                }
+                // verify user
+                else{
+                    // change verified to true
+                    user.verified = true;
+                    user.save(function (err) {
+                        // error occur
+                        if(err){
+                            return res.status(500).send({msg: err.message});
+                        }
+                        // account successfully verified
+                        else{
+                          return res.status(200).send('Your account has been successfully verified');
+                        }
+                    });
+                }
+            });
+        }
+        
+    });
+})
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,8 +66,7 @@ app.use(cors());
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    playground: true,
-    tracing: true
+    playground: true
 });
 // server.express.use(function(req, res, next) {
 //     res.header('Access-Control-Allow-Origin', 'http://localhost:7777');
